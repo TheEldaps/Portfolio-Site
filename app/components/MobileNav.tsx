435dd76fc2f9f7567353a1364e2b492a79992d62
Eldaps <adejames97@gmail.com>
2026-01-07 23:27:30 +0100

    Complete Navigation structure for mobile and desktop

--- app/components/MobileNav.tsx
+++ app/components/MobileNav.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from "next/image"
 import { useState } from "react"
 import NavItems from "./NavItems"
@@ -9,14 +11,17 @@ export default function MobileNav() {
         <section>
             <div className='flex justify-between items-center px-2 sm:hidden'>
                 <Image src='/logo' alt='website logo' width={50} height={50} className='border-2 border-black' ></Image>
-                <button className='h-10 p-1 border-2 flex flex-col gap-1 cursor-pointer hover:bg-[#434141] hover:border-[#0e0e0e66] '>
+                <button
+                    onClick={() => {
+                        setOpenNav(prev => !prev)
+                    }} className='h-10 p-1 border-2 flex flex-col gap-1 cursor-pointer hover:bg-[#434141] hover:border-[#0e0e0e66] '>
                     <div className='bg-black block h-2 w-8'></div>
                     <div className='bg-black block h-2 w-8 '></div>
                     <div className='bg-black block h-2 w-8 '></div>
                 </button>
             </div>
             {openNav && (
-                <ul>
+                <ul className='flex flex-col gap-2 border-2 sm:hidden'>
                     <NavItems title='Home' path='/home' />
                     <NavItems title='About' path='/about' />
                     <NavItems title='Services' path='/service' />
