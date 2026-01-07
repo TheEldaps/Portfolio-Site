f536d6a481365f8435e8d1e95583932e77a29619
Eldaps <adejames97@gmail.com>
2026-01-08 00:57:28 +0100

    Add hero component

---
+++ app/components/Hero.tsx
@@ -0,0 +1,17 @@
+
+
+export default function Hero() {
+    return (
+        <section className='h-screen flex items-center justify-center'>
+
+            <h1>Hi I'm James Oluwatobiloba. A Frontend Developer </h1>
+            <p>I craft modern responsive web apps with clean UI </p>
+            <div>
+                <button>Hire me</button>
+                <button>Download CV</button>
+                <button>View my work
+                </button>
+            </div>
+        </section>
+    )
+}
