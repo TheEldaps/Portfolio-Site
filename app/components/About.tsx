177246b2b1e02e236fa16565666e71f84a802805
Eldaps <adejames97@gmail.com>
2026-01-08 01:27:40 +0100

    Add About component

---
+++ app/components/About.tsx
@@ -0,0 +1,23 @@
+
+export default function About() {
+    return (
+        <section className='flex'>
+            <div></div>
+            <div>
+                <h2 className='underline'>About <span className='text-[purple]'>Me</span></h2>
+                <p>the ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronor
+                    the ipsum dolor mono chronorthe ipsum dolor monochronorthe ipsum dolor mono chronorthe ipsum dolor mono chronor
+                </p>
+
+                <p>the ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronorthe ipsum dolor mono chronor</p>
+
+                <div>
+                    <article>3 years experience</article>
+                    <article>6 Projects Completed</article>
+                    <article><span className='text-[pink]'>100%</span> client satisfaction</article>
+                </div>
+            </div>
+
+        </section>
+    )
+}
