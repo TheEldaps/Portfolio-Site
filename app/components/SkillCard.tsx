6de0adc6b8b7390cb0c41a27ca7b387128b8b07a
Eldaps <adejames97@gmail.com>
2026-01-08 20:43:19 +0100

    style logo technology used

--- app/components/SkillCard.tsx
+++ app/components/SkillCard.tsx
@@ -3,7 +3,7 @@ import { StaticImageData } from "next/image"
 
 export default function SkillCard({ skill, image }: { skill: string, image: StaticImageData }) {
     return (
-        <div className='h-[22vh] w-[40vw] max-w-60 max-h-40 py-2 bg-[#111827cc] backdrop-blur- border border-[#ffffff24] rounded-2xl shadow-lg '>
+        <div className='flex flex-col justify-center h-[22vh] w-[40vw] max-w-60 max-h-40 py-2 bg-[#111827cc] backdrop-blur-lg border border-[#ffffff24] rounded-2xl shadow-lg '>
 
             <Image src={image} width={400} height={400} alt='Skill logo' className='w-[50%] h-auto block  mx-auto' />
             <h4 className='text-center mt-2'>{skill}</h4>
--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,13 +1,13 @@
 import SkillCard from "./SkillCard"
 import GsapLogo from '../../public/Gsaplogo.png'
 import CSSLogo from '../../public/CSSLogo.png'
-import FigmaLogo from '../../public/Figmalogo.png'
+import FigmaLogo from '../../public/FigmaLogo.png'
 import HTMLLogo from '../../public/HTMLlogo.png'
 import JavascriptLogo from '../../public/JavascriptLogo.png'
 import NextLogo from '../../public/NextLogo.png'
 import NodeLogo from '../../public/NodeLogo.png'
 import ReactLogo from '../../public/ReactLogo.png'
-import ReactFormLogo from '../../public/ReactFormlogo.png'
+import ReactFormLogo from '../../public/ReactFormLogo.png'
 import ReactRouterLogo from '../../public/ReactRouterLogo.png'
 import TailwindLogo from '../../public/TailwindCSSLogo.png'
 import ZodLogo from '../../public/ZodLogo.png'
@@ -17,12 +17,12 @@ import ZodLogo from '../../public/ZodLogo.png'
 
 export default function Skills() {
     return (
-        <section className='px-5 h-[100vh]'>
+        <section className='px-5 min-h-[100vh] mt-20'>
             <h2 className='text-[#F8FAC]  text-3xl mb-2'>My Skills</h2>
 
-            <p className='text-[#94a3b8]'>As Frontend developer, I use modern tools and technologies to build fast, responsive and visually appealing web applications</p>
+            <p className='text-[#94a3b8] mb-10'>As Frontend developer, I use modern tools and technologies to build fast, responsive and visually appealing web applications</p>
 
-            <div className='flex flex-wrap gap-[5%]'>
+            <div className='flex flex-wrap gap-[5%] gap-y-12.5'>
                 <SkillCard skill='HTML' image={HTMLLogo} />
                 <SkillCard skill='CSS' image={CSSLogo} />
                 <SkillCard skill='Javascript' image={JavascriptLogo} />
@@ -36,7 +36,7 @@ export default function Skills() {
                 <SkillCard skill='Zod' image={ZodLogo} />
                 <SkillCard skill='React Hook Form' image={ReactFormLogo} />
                 <SkillCard skill='Gsap' image={GsapLogo} />
-                {/* <SkillCard skill='Figma' /> */}
+                <SkillCard skill='Figma' image={FigmaLogo} />
             </div>
         </section>
     )
