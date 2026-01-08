3babdad50fda8d97732b88466a7ed77564030e3f
Eldaps <adejames97@gmail.com>
2026-01-08 20:34:20 +0100

    Add Logos for technology used for design

--- app/components/SkillCard.tsx
+++ app/components/SkillCard.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image"
+import { StaticImageData } from "next/image"
 
-
-export default function SkillCard() {
+export default function SkillCard({ skill, image }: { skill: string, image: StaticImageData }) {
     return (
-        <div>SkillCard</div>
+        <div className='h-[22vh] w-[40vw] max-w-60 max-h-40 py-2 bg-[#111827cc] backdrop-blur- border border-[#ffffff24] rounded-2xl shadow-lg '>
+
+            <Image src={image} width={400} height={400} alt='Skill logo' className='w-[50%] h-auto block  mx-auto' />
+            <h4 className='text-center mt-2'>{skill}</h4>
+        </div>
     )
 }
--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,8 +1,43 @@
+import SkillCard from "./SkillCard"
+import GsapLogo from '../../public/Gsaplogo.png'
+import CSSLogo from '../../public/CSSLogo.png'
+import FigmaLogo from '../../public/Figmalogo.png'
+import HTMLLogo from '../../public/HTMLlogo.png'
+import JavascriptLogo from '../../public/JavascriptLogo.png'
+import NextLogo from '../../public/NextLogo.png'
+import NodeLogo from '../../public/NodeLogo.png'
+import ReactLogo from '../../public/ReactLogo.png'
+import ReactFormLogo from '../../public/ReactFormlogo.png'
+import ReactRouterLogo from '../../public/ReactRouterLogo.png'
+import TailwindLogo from '../../public/TailwindCSSLogo.png'
+import ZodLogo from '../../public/ZodLogo.png'
+
+
+
 
 export default function Skills() {
     return (
-        <section className='px-5'>
-            <h3 className='text-3xl'>My  <span className='text-[purple] '>Skills</span></h3>
+        <section className='px-5 h-[100vh]'>
+            <h2 className='text-[#F8FAC]  text-3xl mb-2'>My Skills</h2>
+
+            <p className='text-[#94a3b8]'>As Frontend developer, I use modern tools and technologies to build fast, responsive and visually appealing web applications</p>
+
+            <div className='flex flex-wrap gap-[5%]'>
+                <SkillCard skill='HTML' image={HTMLLogo} />
+                <SkillCard skill='CSS' image={CSSLogo} />
+                <SkillCard skill='Javascript' image={JavascriptLogo} />
+                <SkillCard skill='React' image={ReactLogo} />
+                <SkillCard skill='Tailwind' image={TailwindLogo} />
+                <SkillCard skill='Next' image={NextLogo} />
+                <SkillCard skill='React Router' image={ReactRouterLogo} />
+                {/* <SkillCard skill='Github' image={ GithubLogo} /> */}
+                <SkillCard skill='Nodejs' image={NodeLogo} />
+                {/* <SkillCard skill='Material UI' image={ MaterialLogo} /> */}
+                <SkillCard skill='Zod' image={ZodLogo} />
+                <SkillCard skill='React Hook Form' image={ReactFormLogo} />
+                <SkillCard skill='Gsap' image={GsapLogo} />
+                {/* <SkillCard skill='Figma' /> */}
+            </div>
         </section>
     )
 }
